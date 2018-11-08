6ab419c5a0e988603d18625ffda8dda257a91208
Kevin Hennessy <kchennessy@wingate.edu>
2018-11-08 15:44:03 -0500

    Added starter code to Listings Component

---
+++ src/Listings.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Listings extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default Listings;
