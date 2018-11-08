a78d1d64f0b83b68312b311926688df2e9d99c82
Kevin Hennessy <kchennessy@wingate.edu>
2018-11-08 16:15:36 -0500

    Added navigation list to Menu component

--- src/Menu.js
+++ src/Menu.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 class Menu extends Component {
     render() {
         return (
-            <div></div>
+            <div className='menu-bar'>
+                <nav className='navigation-menu'>
+                    <ul className='top-menu'>
+                        <li className='top-menu-item'>About Me</li>
+                        <li className='top-menu-item'>Listings</li>
+                        <li className='top-menu-item'>Testimonials</li>
+                    </ul>
+                </nav>
+            </div>
         );
     }
 }
