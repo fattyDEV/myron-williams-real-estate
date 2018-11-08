8abaef11bda3c3d3405579ef902ffba4dbb6fbcb
Kevin Hennessy <kchennessy@wingate.edu>
2018-11-08 16:15:52 -0500

    Added Menu component to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
+import Menu from './Menu.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App-container">
         <Header />
+        <Menu />
       </div>
     );
   }
