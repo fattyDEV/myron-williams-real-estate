1e8ad537939a0b7711282b30bf7bc23690f03f2b
Kevin Hennessy <kchennessy@wingate.edu>
2018-11-08 16:07:03 -0500

    changed App class to App-container

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import headerLogo from './images/MW-header-logo1.png';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App-container">
         <header className='header'>
           <div className='header-logo'><img className='header-image' src={headerLogo} alt='header logo' /></div>
         </header>
