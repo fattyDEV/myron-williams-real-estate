6151cd97aba400719d5e6795fe4cb3a384cc2261
Kevin Hennessy <kchennessy@wingate.edu>
2018-11-08 16:30:08 -0500

    Removed div tags from Menu component

--- src/Menu.js
+++ src/Menu.js
@@ -3,15 +3,13 @@ import React, { Component } from 'react';
 class Menu extends Component {
     render() {
         return (
-            <div className='menu-bar'>
-                <nav className='navigation-menu'>
-                    <ul className='top-menu'>
-                        <li className='top-menu-item'>About Me</li>
-                        <li className='top-menu-item'>Listings</li>
-                        <li className='top-menu-item'>Testimonials</li>
-                    </ul>
-                </nav>
-            </div>
+            <nav className='menu-bar'>
+                <ul className='top-menu'>
+                    <li className='top-menu-item'>About Me</li>
+                    <li className='top-menu-item'>Listings</li>
+                    <li className='top-menu-item'>Testimonials</li>
+                </ul>
+            </nav>
         );
     }
 }
