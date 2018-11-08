38fe0f3d6fb36eb4f1693f10d6a0db723c47b3de
Kevin Hennessy <kchennessy@wingate.edu>
2018-11-08 16:04:12 -0500

    Added header image to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import headerLogo from './images/MW-header-logo1.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <header className='header'>
+          <div className='header-logo'><img className='header-image' src={headerLogo} alt='header logo' /></div>
+        </header>
       </div>
     );
   }
