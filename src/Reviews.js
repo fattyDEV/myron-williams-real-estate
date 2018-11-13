efd954c8a0ec37d748aea142ebada61b94e6d5e7
Kevin Hennessy <kchennessy@wingate.edu>
2018-11-13 16:26:11 -0500

    Added Header and Menu components to Reviews component

--- src/Reviews.js
+++ src/Reviews.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import Header from './Header.js';
+import Menu from './Menu.js';
 
 class Reviews extends Component {
     render() {
         return (
-            <div></div>
+            <div className='top-header'>
+                <Header />
+                <Menu />
+                <img className='background-pic' src='https://images.pexels.com/photos/462358/pexels-photo-462358.jpeg?cs=srgb&dl=garden-house-lawn-462358.jpg&fm=jpg' alt='house pic' />
+            </div>
         );
     }
 }
