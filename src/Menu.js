8afa0df0d4b3bab202e01c5df6ebfb345787bb79
Kevin Hennessy <kchennessy@wingate.edu>
2018-11-13 16:19:27 -0500

    Added class to Link components in Menu component

--- src/Menu.js
+++ src/Menu.js
@@ -6,10 +6,10 @@ class Menu extends Component {
         return (
             <nav className='menu-bar'>
                 <ul className='top-menu'>
-                    <li className='top-menu-item'><Link to={'/'}>Home</Link></li>
-                    <li className='top-menu-item'><Link to={'/about-me'}>About Me</Link></li>
-                    <li className='top-menu-item'><Link to={'/find-a-home'}>Find A Home</Link></li>
-                    <li className='top-menu-item'><Link to={'/testimonials'}>Testimonials</Link></li>
+                    <li className='top-menu-item'><Link className='link-item' to={'/'}>Home</Link></li>
+                    <li className='top-menu-item'><Link className='link-item' to={'/about-me'}>About Me</Link></li>
+                    <li className='top-menu-item'><Link className='link-item' to={'/find-a-home'}>Find A Home</Link></li>
+                    <li className='top-menu-item'><Link className='link-item' to={'/testimonials'}>Testimonials</Link></li>
                 </ul>
             </nav>
         );
