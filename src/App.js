b3c958591729d7505f1ba267cdbe068ca0af3150
Kevin Hennessy <kchennessy@wingate.edu>
2018-11-13 08:20:37 -0500

    Added background image as its own node

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
       <div className="App-container">
         <Header />
         <Menu />
+        <img className='background-pic' src='https://images.pexels.com/photos/462358/pexels-photo-462358.jpeg?cs=srgb&dl=garden-house-lawn-462358.jpg&fm=jpg' alt='house pic' />
       </div>
     );
   }
