0996cfe8c71e8d407bac434f0b91c25950a25c04
Kevin Hennessy <kchennessy@wingate.edu>
2018-11-13 10:05:37 -0500

    added Footer componenter to App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
 import Menu from './Menu.js';
+import Footer from './Footer.js';
 
 class App extends Component {
   render() {
@@ -9,6 +10,8 @@ class App extends Component {
         <Header />
         <Menu />
         <img className='background-pic' src='https://images.pexels.com/photos/462358/pexels-photo-462358.jpeg?cs=srgb&dl=garden-house-lawn-462358.jpg&fm=jpg' alt='house pic' />
+        <main className='main'></main>
+        <Footer />
       </div>
     );
   }
