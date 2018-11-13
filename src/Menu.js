cb40be007c17ac7182c7e60a1c982dbdcfd577d6
Kevin Hennessy <kchennessy@wingate.edu>
2018-11-13 13:09:56 -0500

    Added Link to menu items in Menu component

--- src/Menu.js
+++ src/Menu.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Menu extends Component {
     render() {
         return (
             <nav className='menu-bar'>
                 <ul className='top-menu'>
-                    <li className='top-menu-item'>About Me</li>
-                    <li className='top-menu-item'>Listings</li>
-                    <li className='top-menu-item'>Testimonials</li>
+                    <li className='top-menu-item'><Link to={'/about-me'}>About Me</Link></li>
+                    <li className='top-menu-item'><Link to={'/listings'}>Listings</Link></li>
+                    <li className='top-menu-item'><Link to={'/testimonials'}>Testimonials</Link></li>
                 </ul>
             </nav>
         );
