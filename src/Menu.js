32b58662d598e2aabf608555bc75853109204b83
Kevin Hennessy <kchennessy@wingate.edu>
2018-11-13 13:34:33 -0500

    changed link path to find-a-home in Menu component

--- src/Menu.js
+++ src/Menu.js
@@ -7,7 +7,7 @@ class Menu extends Component {
             <nav className='menu-bar'>
                 <ul className='top-menu'>
                     <li className='top-menu-item'><Link to={'/about-me'}>About Me</Link></li>
-                    <li className='top-menu-item'><Link to={'/listings'}>Find A Home</Link></li>
+                    <li className='top-menu-item'><Link to={'/find-a-home'}>Find A Home</Link></li>
                     <li className='top-menu-item'><Link to={'/testimonials'}>Testimonials</Link></li>
                 </ul>
             </nav>
