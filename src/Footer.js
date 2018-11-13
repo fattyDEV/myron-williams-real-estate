8ea18803e25303b999e52497955a4923abec22e7
Kevin Hennessy <kchennessy@wingate.edu>
2018-11-13 10:05:20 -0500

    Added footer information to Footer component

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return (
-            <div></div>
+            <div className='footer'>
+                <p>Myron Williams Real Estate</p>
+                <p>Main St.</p>
+                <p>Wingate, NC 28174</p>
+                <p>704-221-4377</p>
+                <p>myronw447@aol.com</p>
+            </div>
         );
     }
 }
