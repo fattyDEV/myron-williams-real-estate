ddac91ea51c9aa8f3195206c13f4e8faac2ab2c7
Kevin Hennessy <kchennessy@wingate.edu>
2018-11-13 16:26:58 -0500

    Changed path to Reviews component from testimonials to reviews

--- src/Menu.js
+++ src/Menu.js
@@ -9,7 +9,7 @@ class Menu extends Component {
                     <li className='top-menu-item'><Link className='link-item' to={'/'}>Home</Link></li>
                     <li className='top-menu-item'><Link className='link-item' to={'/about-me'}>About Me</Link></li>
                     <li className='top-menu-item'><Link className='link-item' to={'/find-a-home'}>Find A Home</Link></li>
-                    <li className='top-menu-item'><Link className='link-item' to={'/testimonials'}>Testimonials</Link></li>
+                    <li className='top-menu-item'><Link className='link-item' to={'/reviews'}>Testimonials</Link></li>
                 </ul>
             </nav>
         );
